bea021c757befa3ef225bc9c387124ca114238ae
Will Smith <19693578@students.lincoln.ac.uk>
2020-03-02 13:50:44 +0000

    Created Project

---
+++ CMP1124M/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMP1124M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
